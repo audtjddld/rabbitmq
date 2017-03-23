c383c68dce7b7c42726fe8b6801b9b7a03e68fd8
ichatt_Developer <ichatt_Developer@DESKTOP-VVTI0RA>
2017-03-23 21:04:07 +0900

    performance test

--- ichatRabbitMQ/src/main/java/rabbit/mq/tutorials/duability/DurableSend.java
+++ ichatRabbitMQ/src/main/java/rabbit/mq/tutorials/duability/DurableSend.java
@@ -9,29 +9,29 @@ import com.rabbitmq.client.MessageProperties;
 
 public class DurableSend {
 
-	private final static String QUEUE_NAME = "task_queue";
+	private final static String QUEUE_NAME = "task_queue1";
 
 	public static void main(String[] args) throws Exception {
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
+		
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
 
-		channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 		long num = 0L;
 		while(true) {
-			String message = "hello world --> " + num;
-			channel.basicPublish("", QUEUE_NAME, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes("UTF-8"));
+			String message = num +":hello world";
+			channel.basicPublish("", QUEUE_NAME, null , message.getBytes("UTF-8"));
 			System.out.println(" [x] Sent '" + message + "'");
 			num ++;
 			
-			TimeUnit.MILLISECONDS.sleep(10);
-			if(num == 10000) {
+			if(num == 200000L) {
 				break;
 			}
 		}
 		
-		//channel.close();
-		//connection.close();
+		channel.close();
+		connection.close();
 	}
 }
--- ichatRabbitMQ/src/main/java/rabbit/mq/tutorials/duability/Worker.java
+++ ichatRabbitMQ/src/main/java/rabbit/mq/tutorials/duability/Worker.java
@@ -1,6 +1,8 @@
 package rabbit.mq.tutorials.duability;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
@@ -11,15 +13,18 @@ import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
 
 public class Worker {
-	private final static String QUEUE_NAME = "task_queue";
+	private final static String QUEUE_NAME = "task_queue1";
+
+	private static Map<String, Object> map = new HashMap<>();
 
 	public static void main(String[] args) throws Exception {
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
+
 		final Connection connection = factory.newConnection();
 		final Channel channel = connection.createChannel();
 
-		channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
 		System.out.println("[*] Waiting for messages, To exit press CTRL+C");
 
@@ -31,30 +36,24 @@ public class Worker {
 					byte[] body) throws IOException {
 				String message = new String(body, "UTF-8");
 				// envelope -> routingKey is quene name.
-				//System.out.println(" [x] Received '" + message + "'" + envelope.getRoutingKey());
-				
-				try {
-					doWork(message);
-				} finally {
-					System.out.println("message " + message + " [x] Done");
-					channel.basicAck(envelope.getDeliveryTag(), false);
-				}
+				// System.out.println(" [x] Received '" + message + "'" +
+				// envelope.getRoutingKey());
+				messageProcess(message);
+				//channel.basicAck(envelope.getDeliveryTag(), false); // <-- 겁나 느림 =_=;
+				System.out.println("message " + message + " [x] Done");
 			}
 
 		};
-		boolean autoAck = false;
+		boolean autoAck = true;
 		channel.basicConsume(QUEUE_NAME, autoAck, consumer);
 	}
 
-	private static void doWork(String message) {
-		for (char ch : message.toCharArray()) {
-			if (ch == '.') {
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException _ignored) {
-					Thread.currentThread().interrupt();
-				}
-			}
+	public static void messageProcess(String message) {
+		String key = message.split(":")[0];
+		if (!map.containsKey(key)) {
+			map.put(key, message);
+		} else if (map.containsKey(key)) {
+			System.err.println("message duplicate error!! " + message);
 		}
 	}
 }
